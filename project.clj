2418d49becdaae5a404c54babcd21ac720cfbed8
Jerzy Wieczorek <jerzywie@razzydo.co.uk>
2017-07-03 13:17:55 +0100

    Update ancient clojure and clj-http dependencies

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject essen-basket "0.0.4-SNAPSHOT"
+(defproject essen-basket "0.0.4"
   :description "essen-basket : Essential trading basket/order reader."
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-http "0.7.2"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-http "3.6.1"]
                  [enlive "1.1.1"]
                  [org.clojure/tools.cli "0.3.1"]]
   :profiles {:dev {:dependencies [[midje "1.5.0"]]}
