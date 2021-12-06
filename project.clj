2e51ec972e917452ccfcd9228e1a4ec1f8e7abe3
Jerzy Wieczorek <jerzywie@razzydo.co.uk>
2021-12-06 11:12:12 +0000

    Bump project version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject essen-basket "0.0.5"
+(defproject essen-basket "0.0.6"
   :description "essen-basket : Essential trading basket/order reader."
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-http "3.6.1"]
